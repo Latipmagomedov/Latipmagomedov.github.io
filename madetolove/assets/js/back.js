7ddb5dea8d0dba60e04148390bf77b9aab6e7e13
Latip <37000009+Latipmagomedov@users.noreply.github.com>
2021-08-28 16:46:51 +0300

    Delete madetolove directory

--- madetolove/assets/js/back.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector(".header__mobile-icon");
-const backBtn = document.querySelector(".header__mobile-back");
-
-menuIcon.style.display = "none";
-backBtn.classList.add("header__mobile-back_active");
-
-backBtn.addEventListener("click", () => {
-    history.back();
-});
--- madetolove/assets/js/cart.js
+++
@@ -1,8 +0,0 @@
-const cartInput = document.querySelector(".cart-oninput");
-const cartPush = document.querySelector(".cart__footer-top-push");
-const cartCheck = document.querySelector(".cart__footer-top-check");
-
-cartInput.addEventListener("input", () => {
-    cartPush.classList.remove('hidden')
-    cartCheck.classList.add('hidden')
-});
